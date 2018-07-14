a717139e119e51e559eb3bc63e955361954d2975
MasterCoderK <8674856+DiamondGo@users.noreply.github.com>
2018-07-14 10:19:28 -0700

    modify pkg file

--- setup.py
+++ setup.py
@@ -1,14 +1,25 @@
-from distutils.core import setup
+import setuptools
 
-setup(
-    name = 'easyfunc',
-    packages = ['easyfunc'],
-    version = '0.2',
-    description = 'Easy and simple functional programming style helper for python.',
-    author = 'Key',
-    author_email = 'orangewar@hotmail.com',
-    url = 'https://github.com/DiamondGo/easyfunc',
-    download_url = 'https://github.com/DiamondGo/easyfunc/archive/0.2.tar.gz',
-    keywords = ['fp', 'python', 'functional'],
-    classifiers = [],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="easyfunc",
+    version="0.2.2",
+    author="K",
+    author_email="mastercoderk@gmail.com",
+    description="Easy and simple functional programming style helper for python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DiamondGo/easyfunc",
+    exclude_package_data={'': ['test_easyfunc.py']},
+    packages=setuptools.find_packages(),
+    keywords=['fp', 'python', 'functional'],
+    classifiers=(
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v2 (GPLv2)",
+        "Operating System :: OS Independent",
+    ),
 )
