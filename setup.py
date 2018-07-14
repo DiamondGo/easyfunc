fed54b4bfbbbebbb9c5bb4ff7f857690ae4e23be
Ke Xie <kexi@amazon.com>
2018-07-14 00:03:24 -0700

    add pkg files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name = 'easyfunc',
+    packages = ['easyfunc'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Easy and simple functional programming style helper for python.',
+    author = 'Key',
+    url = 'https://github.com/DiamondGo/easyfunc',
+    download_url = 'https://github.com/DiamondGo/easyfunc/archive/0.1.tar.gz',
+    keywords = ['fp', 'python', 'functional'],
+    classifiers = [],
+)
