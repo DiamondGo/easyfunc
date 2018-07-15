f2241d8d8905e3e3c04d6dc52df342f41af102e2
MasterCoderK <8674856+DiamondGo@users.noreply.github.com>
2018-07-14 19:00:39 -0700

    fix import on py 2

--- easyfunc/__init__.py
+++ easyfunc/__init__.py
@@ -1,4 +1,9 @@
-from . import easyfunc
-from easyfunc.easyfunc import *
+import sys
+if sys.version_info[0] == 2:
+    import easyfunc
+    from easyfunc import *
+elif sys.version_info[0] == 3:
+    from . import easyfunc
+    from easyfunc.easyfunc import *
 
 name = 'easyfunc'
